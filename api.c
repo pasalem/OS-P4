74452dc1646445360ce7d0671048497f88023c15
alec12094 <abenson@wpi.edu>
2015-02-22 22:52:58 -0500

    Progress

--- api.c
+++ api.c
@@ -1,52 +1,112 @@
 #include "api.h"
 // Delay times also occur when values are written to these levels
 
-int main(){
-	
-}
-
 vAddr allocateNewInt();
 int * accessIntPtr (vAddr address);
 void unlockMemory(vAddr address);
 void freeMemory(vAddr address);
+void addPage(int level, vAddr *location);
 
 
 // Reserves memory location, sizeof(int)
 // Must be created in emulated RAM, pushing other pages 
-	// into lower layers of hierarchy, if full
+// into lower layers of hierarchy, if full
 // Return -1 if no memory available
 vAddr allocateNewInt(){
+	if( page_count > SIZE_PAGE_TABLE ){
+		//Nothing is free!
+		printf("The page table is full. Free some pages\n");
+		return -1;
+	}
 
-
+	if(ram_count < SIZE_RAM ){
+		//RAM has a free spot
+		RAM[ram_count] = ram_count;
+		addPage(RAM_LEVEL, &RAM[ram_count]);
+	} else if(ssd_count < SIZE_SSD){
+		//SSD has a free spot
+		SSD[ssd_count] = ssd_count;
+		addPage(SSD_LEVEL, &SSD[ssd_count]);
+	} else if(hdd_count < SIZE_HDD){
+		//HDD has a free spot
+		HDD[hdd_count] = hdd_count;
+		addPage(HDD_LEVEL, &HDD[hdd_count]);
+	} else{
+		//Nothing is free!
+		printf("The entire heirarchy has been filled %d %d %d %d\n", ram_count, ssd_count, hdd_count, page_count);
+	}
 }
-
 
 // Obtains indicated memory page
 // Returns an int pointer to the location in emulated RAM
 // Page locked in memory and considered "dirty"
 // Returns NULL if pointer cannot be provided 
-	// (if page must be brought into RAM, and all RAM is locked)
+// (if page must be brought into RAM, and all RAM is locked)
 int * accessIntPtr (vAddr address){
-
-
 }
-
 
 // Memory must be unlocked when user is done with it
 // Swap to disk if needed
 // Any previous pointers in memory are considered invalid and can't be used
-	// if user calls this function
+// if user calls this function
 void unlockMemory(vAddr address){
-
-
 }
-
 
 // User can free memory when user is done with the memory page
 // Frees page in memory, and deletes any swapped out copies of page
 void freeMemory(vAddr address){
+	int index = 0;
+	for(index = 0; index < SIZE_PAGE_TABLE; index++){
+		if( *page_table[index].location == address){
+			page_table[index].allocated = 0;
+		}
+	}
+}
 
+void addPage(int level, vAddr *location){
+	page new_page;
+	new_page.location = location;	//Page refers to this spot in memory
+	new_page.locked = 0;			//Page is unlocked by default
+	new_page.referenced = 0;		//Page is unreferenced by default
+	new_page.allocated = 1;			//Page is allocated by default
+	new_page.level = level;
+	page_table[page_count] = new_page;
 
+	switch(level){
+		case RAM_LEVEL:
+			ram_count++;
+			break;
+		case SSD_LEVEL:
+			ssd_count++;
+			break;
+		case HDD_LEVEL:
+			hdd_count++;
+			break;
+	}
+	page_count++;
+	printf("Added page %d\n", page_count);
+}
+
+//Allocate, access, update, unlock, and free memory
+//As we call allocateNewInt, we need to swap old pages to secondary memory
+void memoryMaxer() {
+	vAddr indexes[SIZE_PAGE_TABLE];
+	int index = 0;
+	for (index = 0; index < SIZE_PAGE_TABLE; ++index) {
+		indexes[index] = allocateNewInt();
+		//int *value = accessIntPtr(indexes[index]);
+		//*value = (index * 3);
+		unlockMemory(indexes[index]);
+	}
+
+	for (index = 0; index < SIZE_PAGE_TABLE; ++index) {
+		freeMemory(indexes[index]);
+	}
+}
+
+//Run the actual memory management tool
+int main(){
+	memoryMaxer();
 }
 
 
--- api.h
+++ api.h
@@ -1,9 +1,35 @@
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define MAX_VADDRESSES 1000	// System limited to 1000 virtual memory pages
-#define MAX_RAM 25			// Access time immediate
-#define MAX_SSD 1000 		// Access time usleep 0.25 sec
-#define MAX_DISK 1000		// Access time usleep 2.5 sec
+#define SIZE_RAM 25			// Access time immediate
+#define SIZE_SSD 100 		// Access time usleep 0.25 sec
+#define SIZE_HDD 1000		// Access time usleep 2.5 sec
+#define SIZE_PAGE_TABLE 1000
+#define TRUE 1
+#define FALSE 0
+
+#define RAM_LEVEL 0
+#define SSD_LEVEL 1
+#define HDD_LEVEL 2
 
 typedef signed short vAddr;
+
+typedef struct page{
+	vAddr *location;	//A pointer to the page
+	int locked;			//whether or not the page is locked
+	int referenced;		//Whether or not the page has been recently accessed
+	int allocated;		//Whether or not this page is taken or not
+	int level;
+} page;
+
+
+int ram_count = 0;
+int ssd_count = 0;
+int hdd_count = 0;
+int page_count = 0;
+vAddr RAM[SIZE_RAM];
+vAddr SSD[SIZE_SSD];
+vAddr HDD[SIZE_HDD];
+page page_table[SIZE_PAGE_TABLE];
